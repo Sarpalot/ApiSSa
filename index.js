5ca6a35fed7ba6c2ec2fd202c3871dd65b661965
simo salosara <simo.salosara@gmail.com>
2021-03-03 12:18:20 +0200

    js ja html update

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-app.use(express.static("public"));
-let customers = [
-    { id: 1, name: "Jack" },
-    { id: 2, name: "Tina" },
+
+app.use(express.static('public'));
+
+let database = [
+    { id: 1, latitude: 60, longitude: 20 },
+    { id: 2, latitude: 0, longitude: 0 },
 ];
-// HTTP GET http://localhost:8080/api/customers
-app.get("/api/customers", (req, res) => {
-    res.send(customers);
+
+app.get('/api/locations', (req, res) => {
+    let format = req.query.format;
+    if (format === 'pretty') {
+        res.header('Content-Type', 'application/json');
+        res.send(JSON.stringify(database, null, 4));
+    } else {
+        res.json(database);
+    }
 });
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server listening on port ${port}`);
 });
